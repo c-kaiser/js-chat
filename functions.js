097d02817d69a098bcc19ab082f6aee867ec8c20
aleksandra <aleksandra.juszczak@innoq.com>
2022-10-11 15:03:13 +0200

    arrow functions

--- functions.js
+++ functions.js
@@ -23,3 +23,63 @@ let m = {x:5}
 
 f(m)
 console.log(m);
+
+let f1= function(){
+    this.x = 12
+    let f2 = () => {
+        this.x=13;  
+        //this in arrow function will be the same as this in its environment/context 
+        //(so here we'll have f1.x)
+    }
+    f2();
+    console.log(this.x);
+}
+//1st option instead:
+let f1a= function(){
+    this.x = 12
+    let that=this
+    let f2 = () => {
+        that.x=13;  
+    }
+    f2();
+    console.log(this.x);
+}
+//2nd option instead:
+let f1b= function(){
+    this.x = 12
+    let f2 = (function(){
+        this.x=13;  
+    }).bind(this)
+    f2();
+    console.log(this.x);
+}
+let o= {};
+f.call(o);
+
+//Arrow functions options
+let createMessage = (sender, text, type) =>{
+    return{
+         textBody: text,
+         sender,
+         type,
+         render: render
+     }};
+let createMessage1 = (sender, text, type) =>{
+    return{
+         textBody: text,
+         sender,
+         type,
+         render: render
+     }};
+let createMessage2 = (sender, text, type) =>(
+    {
+         textBody: text,
+         sender,
+         type,
+         render: render
+     });
+let someArrowFunction = (sender, text, type) =>{ sender, text, type}; //here no object is created
+let someArrowFunction1 = (sender, text, type) => sender, text, type; 
+let someArrowFunc= ()=>("sth");
+let someArrowFunc1= ()=>"sth";
+let someArrowFunc2= ()=>{"sth"};
