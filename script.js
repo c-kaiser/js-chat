a5a70b99c44b96db2f1d45925fd7e42d78971e1d
christiank <christian.kaiser@innoq.com>
2022-10-11 18:49:29 +0200

    löse Assignment 4

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const TYPE_USER = "user";
 const TYPE_SYSTEM = "system";
 
-function Message(textBody){ 
-    this.textBody = textBody;
-    this.render = ()=>this.textBody;
+function Message(textBody) {
+  this.textBody = textBody;
+  this.render = () => this.textBody;
 }
-function SystemMessage(textBody){ 
-    Message.call(this, textBody);
+function SystemMessage(textBody) {
+  Message.call(this, textBody);
 
-    this.type = TYPE_SYSTEM;
+  this.type = TYPE_SYSTEM;
 }
-function UserMessage(textBody, sender){ 
-    Message.call(this, textBody);
-    this.sender= sender;
-    this.type = TYPE_USER;
+function UserMessage(textBody, sender) {
+  Message.call(this, textBody);
+  this.sender = sender;
+  this.type = TYPE_USER;
 }
-
 
 let createMessage = (sender, text, type) => ({
   textBody: text,
@@ -27,7 +26,6 @@ let createMessage = (sender, text, type) => ({
 let sendMessage = function (m) {
   console.log(m);
   console.log(m.render());
-
 };
 
 let render = function () {
@@ -39,35 +37,32 @@ let render = function () {
 };
 
 let messages = [
-  new UserMessage("I like pasta","sender1"),
-  new UserMessage("Cool","sender2"),
-  new UserMessage("Nice weather","sender2"),
-  new SystemMessage("sth went wrong")
+  new UserMessage("I like pasta", "sender1"),
+  new UserMessage("Cool", "sender2"),
+  new UserMessage("Nice weather or not?", "sender2"),
+  new SystemMessage("sth went wrong"),
 ];
 
- for (const message of messages) {
-     sendMessage(message);
- }
+for (const message of messages) {
+  sendMessage(message);
+}
 
+let countWords = function (member) {
+  return messages
+    .filter((m) => m.sender === member)
+    .map((m) => m.textBody)
+    .reduce((wordCount, text) => text.split(" ").length + wordCount, 0);
+};
 
+let chatMembers = [
+  ...new Set(messages.filter((m) => m.type === TYPE_USER).map((m) => m.sender)),
+];
 
-// console.log("forEach...")
-// messages.forEach(message => {
-//     sendMessage(message);
-// });
-
-
-/*
-let chatMembers= new Set(
-                messages.filter(m=>m.type===TYPE_USER)
-                .map(m=>m.sender)
-                );
 console.log(chatMembers);
 
-chatMembers.map(member=>(
-    `${member}, ${countWords(member)}`
-))
-let countWords= function(member){
-    // return messages.filter(m=>m.sender===)
-}
-*/
+const wordsByMember = chatMembers.reduce(
+  (result, member) => ({ ...result, ...{ [member]: countWords(member) } }),
+  {}
+);
+
+console.log(wordsByMember);
